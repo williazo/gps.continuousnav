b84aef88cea355901c08f75b952c118db7e37e30
Justin Williams <williazo@ucla.edu>
2017-11-28 10:24:09 -0800

    Added in documentation for the wd_choice() funciton. This is really only for my own personal use, based on the three machines where I am running the analyses.

--- R/wd_function.R
+++ R/wd_function.R
@@ -1,11 +1,19 @@
-#### Working Directory Function for Reading in Files from Different Computers ####
+#' Working Directory Function
+#'
+#' @param comp_choice Character value specifying which of the three computers I am using
+#' @param type_save Character value specifying which type of file to save, e.g. Image
+#'
+#' @return Returns the working directory of interest
+#'
+#' @export
+
 
 #creating a function that will allow me to switch between computers easier
 wd_choice <- function(comp_choice, type_save){
   macbook_wd  <- "~/Documents/UCLA/"
   iMac_wd     <- "~/Documents/Propensity_Project/"
   may_lab_wd  <- "~/PropensityScoreModeling/"
-  if(is.null(type_save)==T){ 
+  if(is.null(type_save)==T){
     #if the type_save argument is null then we assume that we are reading in raw data
     if (comp_choice=="macbook"){
       paste0(macbook_wd, "ECOS Study/")
