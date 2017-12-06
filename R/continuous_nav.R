b02f3b8c1abff38110a7cb433963f59555c56605
Justin Williams <williazo@ucla.edu>
2017-12-06 08:56:51 -0800

    Adding new function to incorporate the dose-response aspect.

---
+++ R/continuous_nav.R
@@ -0,0 +1,17 @@
+#'Estimating the Dose Response Function given the Generalized Propensity Score
+#'
+#'This function estimates the fitted value from a linear hierarchical mixed effects model, with the
+#'generalized propensiy score and treatment occuring at the highest level
+#'
+#'@param formula This is a formula object specifying the dose-response model
+#'@param data This is the data frame used to fit the model
+#'@param intervals Specify the number of intervals to include.
+#'
+#'@import lme4
+#'@export
+
+continuous_nav <- function(formula, data, intervals = 50){
+
+  mm_out <- lme4::lmer(formula, data = dat) #running the mixed effects model using lme4
+
+}
