64575e9c0e1ebd02fe3174d8dd2ab6ec01dfc5ce
Justin Williams <williazo@ucla.edu>
2017-12-08 13:52:28 -0800

    fixed the function so that it can properly call in the normal_gps equation

--- R/continuous_nav.R
+++ R/continuous_nav.R
@@ -5,7 +5,13 @@
 #'
 #'@param formula This is a formula object specifying the dose-response model
 #'@param data This is the data frame used to fit the model
+#'@param tx This is the continuous treatment vector
 #'@param intervals Specify the number of intervals to include.
+#'@param covs Matrix of observed covariates
+#'@param interact_vars Specifies a character subset of the variables from the matrix covs and adds in pairwise interactions between all included covariates
+#'@param polynomial_vars Specifies a character subset of variables from the matrix covs to include as polynomial terms
+#'@param polynomial_deg Scalar value that identifies to what power the polynomial variable should be raised
+#'
 #'
 #'@import lme4
 #'
@@ -13,13 +19,14 @@
 #'
 #'@export
 
-continuous_nav <- function(formula, data, intervals = 50){
+continuous_nav <- function(formula, data, tx, intervals = 50, covs,
+                           interact_vars, polynomial_vars, polynomial_deg){
 
   mm_out <- lme4::lmer(formula, data) #running the mixed effects model using lme4
+  omega_hat <- summary(mm_out)$coefficients
 
   #breaking down the outcome and the RHS
   formula_breakdown <- strsplit(formula, " ~ ")
-
   #breaking apart the RHS by terms
   pred_and_random <- unlist(strsplit(formula_breakdown[[1]][2], "[+]"))
   #extracting the random effects in the formula
@@ -29,4 +36,11 @@ continuous_nav <- function(formula, data, intervals = 50){
   fixed_RHS <- as.formula(paste("~", paste(fixed, collapse = "+")))
 
   design_mat <- model.matrix(fixed_RHS, data)
+
+  tx_values <- seq(min(tx), max(tx), length.out = intervals)
+
+  r_values <- normal_gps(tx, covs, gps_val = tx_values,
+                         interact_vars, polynomial_vars, polynomial_deg)
+
+  result = list(mixed_model = mm_out, coef_est = omega_hat, tx_values, r_values)
 }
